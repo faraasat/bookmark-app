636f4f712961110f58a21ddf0e351e1f2f0eae53
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-19 09:05:11 +0500

    Added Layout Component and Index

---
+++ src/components/small-nav/small-nav.component.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import AddTodoComponent from "../add-todo/add-todo.component"
+import SearchTodoComponent from "../search-todo/search-todo.component"
+import "./small-nav.styles.css"
+
+const SmallNavComponent = () => {
+  return (
+    <div className={"bookmark-component__small-nav"}>
+      <AppBar position="static">
+        <Toolbar className="bookmark-component__alignment">
+          <SearchTodoComponent />
+          <AddTodoComponent />
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default SmallNavComponent
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import Layout from "../components/layout/layout.component"
+import SEO from "../components/seo"
+import "./index.styles.css"
+import SmallNavComponent from "../components/small-nav/small-nav.component"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Index Page" />
+      <div className="bookmark-page__index">
+        <SmallNavComponent />
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
