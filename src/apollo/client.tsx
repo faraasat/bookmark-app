e7d259d11fb62218318c9816cad26dfd633d6700
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-19 09:17:38 +0500

    Added Root Component Wrapper

--- src/apollo/client.tsx
+++ src/apollo/client.tsx
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "/.netlify/functions/todoFunc",
+    uri: "/.netlify/functions/myBookmarks",
     fetch,
   }),
   cache: new InMemoryCache(),
